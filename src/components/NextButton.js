7edbd4b3f955b94dc1838edeba05a9359daa6ae7
Amy Mangor <aemangor@gmail.com>
2021-05-10 12:26:28 -0400

    random question start

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
 export default function NextButton() {
+    const x = Math.floor(Math.random()*11)
+    console.log(x)
     return (
         <div>
             <button>Next</button>
