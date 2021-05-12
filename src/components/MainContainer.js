feab40ca3b9f0e9f2ec75964314123f03e76cd61
Alex Kravecas <akravecas@yahoo.com>
2021-05-12 19:10:27 -0400

    questions rendering

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import Questions from "./QuestionContainer";
 import Profile from "./Profile";
 import NextButton from "./NextButton";
+import QuestionContainer from "./QuestionContainer"
 
 export default class MainContainer extends Component {
   state = {
     id: 1,
     quizData: [],
     question: [],
+    usedQuestions:[],
     answers: [],
     correctanswer: "",
     score: 0,
@@ -18,33 +19,52 @@ export default class MainContainer extends Component {
   componentDidMount() {
     fetch("http://localhost:3000/Questions")
       .then((r) => r.json())
-      .then((quizData) => this.setState({ quizData: quizData }));
+      .then((quizData) => this.setState({ quizData: quizData }))
+      // .then(console.log);
   }
+
 
   setCorrect = () => {
     this.setState({ score: this.state.score + 1 });
   };
 
-  setRandomQuestion = () => {
-    const y = Math.floor(Math.random()*this.state.quizData.length)
-    let i = 0;
-    if (this.state.idSelector.length==this.state.quizData)
-    for (i = 0; i < 11; i ++) {
-        if (!this.state.idSelector.includes(y)) {
-            this.setState({idSelector: [...this.state.idSelector, y]});
-        } else {console.log('yo')}
-    }
-}
+  setQuestion = (num) => {
+    const x = this.state.quizData.splice(num,1)
+    this.state.usedQuestions.includes(x) ? this.getNewRandNum() : this.setState({question: x, usedQuestions: [...this.state.usedQuestions, x]
+    })
+  }
+
+  getNewRandNum = () => {
+    const z = Math.floor(Math.random()*this.state.quizData.length)
+    this.setQuestion(z)
+  }
+
+//   setRandomQuestion = () => {
+//     const y = Math.floor(Math.random()*this.state.quizData.length)
+//     let i = 0;
+//     if (this.state.idSelector.length==this.state.quizData)
+//     for (i = 0; i < 11; i ++) {
+//         if (!this.state.idSelector.includes(y)) {
+//             this.setState({idSelector: [...this.state.idSelector, y]});
+//         } else {console.log('yo')}
+//     }
+// }
+
 
   render() {
+    console.log(this.state.question)
+    console.log(this.state.usedQuestions)
+
     return (
       <div className = "maincont">
         <Profile/>
         <p>This is a quiz</p>
         <div className = "test">
-        <Questions quizData={this.state.quizData} id={this.state.id} setCorrect={this.setCorrect}/>
+        <QuestionContainer question = {this.state.question} quizData = {this.state.quizData} usedQuestions = {this.state.usedQuestions}/>
         </div>
-        <div className ="button"><NextButton /></div>
+        <div className ="button">
+        <NextButton quizData = {this.state.quizData} setQuestion = {this.setQuestion}/>
+          </div>
       </div>
     );
   }
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,22 +1,24 @@
 import React, { Fragment } from "react";
-import Answer from "./Answer";
+// import Answer from "./Answer";
 
 const Question = (props) => {
-  const question = props.question.question;
-  const answers = props.question.answers;
-  const correctanswer = props.question.correctanswer;
-  console.log(props);
+  // const question = props.question.question;
+  // const answers = props.question.answers;
+  // const correctanswer = props.question.correctanswer;
+  // console.log(props);
+  
+  console.log(props.question)
 
   return (
     <Fragment>
-    <div className = "questholder">
-      <div className="question">
-        <span>{question}</span>
+      <div>
+       <div className="question">{props.question?.question}</div >
+        {/* <div className="answers">
+          {props.question.answers.map((answer, idx) => (<ul><li className = "listy"><p key={idx} onClick={() => answer === props.correctanswer ? props.setCorrect() : ""}>{answer}</p></li></ul>))}
+        </div>   */}
       </div>
-      <div className="answers">
-        {answers.map((answer, idx) => (<ul><li className = "listy"><p key={idx} onClick={() => answer === correctanswer ? props.setCorrect() : ""}>{answer}</p></li></ul>))}
-      </div>
-    </div>
+      
+
     </Fragment>
   );
 };
